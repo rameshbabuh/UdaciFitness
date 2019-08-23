b1b8711e4fd38f6b2d462506eb054788b6ff875a
Ramesh <rameshbabu9889@gmail.com>
2019-08-22 21:53:18 -0500

    updates

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { View, TouchableOpacity, Text } from 'react-native'
 import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
+import { Ionicons } from '@expo/vector-icons'
+import TextButton from './TextButton'
 
-function submitBtn({onPress}){
-
+function SubmitBtn({onPress}){
+    return(
+        <TouchableOpacity
+            onPress={onPress}>
+            <Text>SUBMIT</Text>
+        </TouchableOpacity>
+    )
 }
 
 export default class AddEntry extends Component {
   state = {
     run: 0,
-    bike: 0,
+    bike: 10,
     swim: 0,
     sleep: 0,
-    eat: 0
+    eat: 5
   }
   increment = (metric) => {
     const { max, step } = getMetricMetaInfo(metric)
@@ -55,12 +62,30 @@ export default class AddEntry extends Component {
         eat: 0
     }))
   }
+  reset = () => {
+    const key = timeToString();
+  }
   render() {
     const metaInfo = getMetricMetaInfo()
+    if(true){
+        return(
+            <View>
+                <Ionicons
+                    name={"ios-happy"}
+                    size={100}
+                />
+                <Text>You already logged your information for today</Text>
+                <TextButton onPress={this.reset}>
+                    Reset
+                </TextButton>
+            </View>
+        )
+    }
 
     return (
       <View>
         <DateHeader date={(new Date()).toLocaleDateString()} />
+        <Text>{JSON.stringify(this.state)}</Text>
         {Object.keys(metaInfo).map((key) => {
           const { getIcon, type, ...rest } = metaInfo[key]
           const value = this.state[key]
@@ -83,6 +108,7 @@ export default class AddEntry extends Component {
             </View>
           )
         })}
+        <SubmitBtn onPress={this.submit}/>
       </View>
     )
   }
--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,10 +1,20 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Slider, Text } from 'react-native'
 
-export default function UdaciSlider(){
+export default function UdaciSlider({max, unit, step, value, onChange}){
     return (
         <View>
-            <Text>UdaciSlider</Text>
+            <Slider 
+                step={step}
+                maximumValue={max}
+                minimumValue={0}
+                value={value}
+                onValueChange={onChange}
+            />
+            <View>
+                <Text>{value}</Text>
+                <Text>{unit}</Text>
+            </View>
         </View>
     )
 }
